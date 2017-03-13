10df512f1c2084fb020f8687b7613dd7a2e62b98
侯群祥 <179127439@qq.com>
2017-03-13 19:48:28 +0800

    Add files via upload

---
+++ Test1_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Test1_1{
+	public static void main(String[]args){
+		Scanner abc=new Scanner(System.in);
+		System.out.println("ÊäÈëµ×°ë¾¶ºÍ¸ß");
+		int num1=abc.nextInt();
+		int num2=abc.nextInt();
+		System.out.println(3.14*num1*num1*num2);
+
+
+	}
+
+
+
+}
